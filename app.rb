4e697e190b1e4358342d05ea1bd9800626239d7c
alexHoggett <alexmh99@gmail.com>
2023-01-17 13:52:09 +0000

    need to run rake db:create

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'bcrypt'
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+  register Sinatra::ActiveRecordExtension
 
   get '/' do
     return erb(:index)
   end
+end
